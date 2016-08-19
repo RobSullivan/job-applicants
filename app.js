4aa5733051425a287ea9824508d330bc234dbd3b
Robert Sullivan <robertjsullivan.esq@gmail.com>
2016-08-19 02:24:50 +0100

    Removed node-sass-middleware from app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+
 
 var routes = require('./routes/index');
 
